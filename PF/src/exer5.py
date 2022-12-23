8dd77ca52d4e1c4419b9036c435ec4e51e048293
Lucas-Barrigo <licaslicas20@gmail.com>
2022-12-23 15:23:15 +0000

    Upgrade

--- PF/src/exer5.py
+++ PF/src/exer5.py
@@ -28,19 +28,27 @@ def ConvertIdadeToNum(idadestr):
     else:
         return "UNKOW"
 
+def ConvertSinaisLuminosos(carga):
+    if carga == "Inexistentes":
+        return 0
+    elif carga == "A funcionar normalmente":
+        return 1
+    else:
+        return "UNKOW"
+
 regAcidentes = ReadJson()
-csvStr = "cvAnomatricula,aVelocidadelocal,cvSexo,cvIdade\n"
+csvStr = "cvAnomatricula,aVelocidadelocal,cvSexo,cvIdade,aVelocidadegeral,aSinaisLuminosos\n"
 for dado in regAcidentes:
     try:
-        x = str(int(dado.condutoresVeiculos[0].Anomatricula)) + "," + str(int(dado.acidentes[0].Velocidadelocal)) + "," + str(int(ConvertSexoToNum(dado.condutoresVeiculos[0].Sexo))) + "," + str(int(ConvertIdadeToNum(dado.condutoresVeiculos[0].Idade))) + "\n"
+        x = str(int(dado.condutoresVeiculos[0].Anomatricula)) + "," + str(int(dado.acidentes[0].Velocidadelocal)) + "," + str(int(ConvertSexoToNum(dado.condutoresVeiculos[0].Sexo))) + "," + str(int(ConvertIdadeToNum(dado.condutoresVeiculos[0].Idade))) + "," + str(int(dado.acidentes[0].Velocidadelocal))+ "," + str(int(ConvertSinaisLuminosos(dado.acidentes[0].SinaisLuminosos))) + "\n"
         csvStr += x  
     except:
         a=0
 
-with open("C:/AAESCOLA/AprendizagemAutomática/PF/src/exer5Data.csv", "w", encoding='utf-8') as file:
+with open("D:/tudo/Mestrado/IAA/Final/AprendizagemAutomatica/PF/exer5Data.csv", "w", encoding='utf-8') as file:
     file.write(csvStr)
 
-df = pd.read_csv(r'C:/AAESCOLA/AprendizagemAutomática/PF/src/exer5Data.csv' )
+df = pd.read_csv(r'D:/tudo/Mestrado/IAA/Final/AprendizagemAutomatica/PF/exer5Data.csv' )
 
 correlations = df.corr()
 sns.heatmap(correlations)
