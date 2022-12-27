3c94992d18ceff69e9f0462c0be9b58e9a382fed
Lucas-Barrigo <licaslicas20@gmail.com>
2022-12-27 10:37:22 +0000

    Update Feridos

--- PF/src/exer5.py
+++ PF/src/exer5.py
@@ -36,6 +36,34 @@ def ConvertSinaisLuminosos(carga):
     else:
         return "UNKOW"
 
+def ConvertTipoPiso(carga):
+    if carga == "Betuminoso":
+        return 0
+    elif len(carga) == 7: #calçada
+        return 1
+    elif carga == "Terra batida":
+        return 2
+    else:
+        return "UNKOW"
+
+def ConvertTipoVias(carga):
+    if carga == "A  - Auto-Estrada":
+        return 0
+    elif len(carga) > 20 : #IC - Itinerário Complementar
+        return 1
+    elif carga == "Arruamento":
+        return 2
+    elif carga == "EM - Estrada Municipal":
+        return 3
+    elif carga == "EN - Estrada Nacional":
+        return 4
+    elif carga == "EF - Estrada Florestal":
+        return 5
+    elif carga == "Outra Via":
+        return 6
+    else:
+        return "UNKOW"
+
 def ConvertDiaDaSemana(carga):
     if carga == "Domingo      ":
         return 0
@@ -55,10 +83,10 @@ def ConvertDiaDaSemana(carga):
         return "UNKOW"
 
 regAcidentes = ReadJson()
-csvStr = "cvAnomatricula,aVelocidadelocal,cvSexo,cvIdade,aSinaisLuminosos,aDiadaSemana\n"
+csvStr = "cvAnomatricula,aVelocidadelocal,aVelocidadegeral,cvSexo,cvIdade,aSinaisLuminosos,aDiadaSemana,aTipoPiso,aTipoVias,aNumFeridosgravesa30dias,aNumFeridosligeirosa30dia\n"
 for dado in regAcidentes:
     try:
-        x = str(int(dado.condutoresVeiculos[0].Anomatricula)) + "," + str(int(dado.acidentes[0].Velocidadelocal)) + "," + str(int(ConvertSexoToNum(dado.condutoresVeiculos[0].Sexo))) + "," + str(int(ConvertIdadeToNum(dado.condutoresVeiculos[0].Idade))) + "," + str(int(ConvertSinaisLuminosos(dado.acidentes[0].SinaisLuminosos))) + "," + str(int(ConvertDiaDaSemana(dado.acidentes[0].DiadaSemana))) +"\n"
+        x = str(int(dado.condutoresVeiculos[0].Anomatricula)) + "," + str(int(dado.acidentes[0].Velocidadelocal)) + "," + str(int(dado.acidentes[0].Velocidadegeral)) + "," + str(int(ConvertSexoToNum(dado.condutoresVeiculos[0].Sexo))) + "," + str(int(ConvertIdadeToNum(dado.condutoresVeiculos[0].Idade))) + "," + str(int(ConvertSinaisLuminosos(dado.acidentes[0].SinaisLuminosos))) + "," + str(int(ConvertDiaDaSemana(dado.acidentes[0].DiadaSemana))) + "," + str(int(ConvertTipoPiso(dado.acidentes[0].TipoPiso))) + "," + str(int(ConvertTipoVias(dado.acidentes[0].TiposVias)))+ "," + str(int(dado.acidentes[0].Velocidadelocal)) + "," + str(int(dado.acidentes[0].NumFeridosgravesa30dias)) + "," + str(int(dado.acidentes[0].NumFeridosligeirosa30dias)) +"\n"
         csvStr += x  
     except:
         a=0
